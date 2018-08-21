27e33c2b1f74c99d566d7172e17bc0279666e26c
jenzu20 <rogeliogido@gmail.com>
2018-08-21 11:58:27 -0400

    newer

--- resources/views/bid.blade.php
+++ resources/views/bid.blade.php
@@ -104,6 +104,48 @@
 			</div>
 		</div>
 	</div> 
+	<div class="modal fade" id="Modal" role="dialog">
+	    <div class="modal-dialog">
+	    
+	      <!-- Modal content-->
+	      <div class="modal-content">
+	        <div class="modal-header inq">
+	          <button type="button" class="close" data-dismiss="modal">&times;</button>
+	          <h4 class="modal-title">Inquiries</h4>
+	        </div>
+	        <div class="modal-body">
+	          <textarea placeholder="Enter your query here" rows="4" cols="50"></textarea>
+	        </div>
+	        <div class="modal-footer">
+	          	<div class="text-center">
+					<!-- Button HTML (to Trigger Modal) -->
+					<a style="color: #fff;" href="#mode" class="trigger-btn" data-toggle="modal">Submit Tender Query</a>
+				</div>
+	          	<div id="mode" class="modal fade">
+					<div class="modal-dialog modal-confirm">
+						<div class="modal-content">
+							<div class="modal-head">
+								<div class="icon-box">
+									<i class="material-icons">done</i>
+								</div>				
+								<h4 class="modal-title">Are  you  sure  you  want  to  submit  this  tender  query? </h4>	
+				                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+							</div>
+							<div class="modal-body">
+								<p>Your  query  will  be  sent  to  the  Employer  and  published  on  the  project  page,  visible  for  everyone,  once  you  have  received  a  response. This process cannot be undone.</p>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-info" data-dismiss="modal">Go Back</button>
+								<button type="button" class="btn btn-success">Submit</button>
+							</div>
+						</div>
+					</div>
+				</div>
+	        </div>
+	      </div>
+	      
+	    </div>
+	  </div>
 
 	<div class="container below-header">
 		<h1 class="project-title bid-page-title">Canada Water Masterplan - <small>Structural Engineer</small></h1>			
@@ -125,48 +167,7 @@
 				</div>
 				<div class="form-group">
 					<button type="button" class="btn btn-info btn-lg raise" id="but" data-toggle="modal" data-target="#Modal">Ask the Employer</button>
-					<div class="modal fade" id="Modal" role="dialog">
-					    <div class="modal-dialog">
-					    
-					      <!-- Modal content-->
-					      <div class="modal-content">
-					        <div class="modal-header inq">
-					          <button type="button" class="close" data-dismiss="modal">&times;</button>
-					          <h4 class="modal-title">Inquiries</h4>
-					        </div>
-					        <div class="modal-body">
-					          <textarea placeholder="Enter your query here" rows="4" cols="50"></textarea>
-					        </div>
-					        <div class="modal-footer">
-					          	<div class="text-center">
-									<!-- Button HTML (to Trigger Modal) -->
-									<a style="color: #fff;" href="#mode" class="trigger-btn" data-toggle="modal">Submit Tender Query</a>
-								</div>
-					          	<div id="mode" class="modal fade">
-									<div class="modal-dialog modal-confirm">
-										<div class="modal-content">
-											<div class="modal-head">
-												<div class="icon-box">
-													<i class="material-icons">done</i>
-												</div>				
-												<h4 class="modal-title">Are  you  sure  you  want  to  submit  this  tender  query? </h4>	
-								                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-											</div>
-											<div class="modal-body">
-												<p>Your  query  will  be  sent  to  the  Employer  and  published  on  the  project  page,  visible  for  everyone,  once  you  have  received  a  response. This process cannot be undone.</p>
-											</div>
-											<div class="modal-footer">
-												<button type="button" class="btn btn-info" data-dismiss="modal">Go Back</button>
-												<button type="button" class="btn btn-success">Submit</button>
-											</div>
-										</div>
-									</div>
-								</div>
-					        </div>
-					      </div>
-					      
-					    </div>
-					  </div>
+					
 				</div>
 			</div>
 			<div class="col-sm-8 questionnaire-section" style="margin-left: 400px;">
