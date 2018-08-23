3ecd7d7220b5483921a28b32e7adb20861777438
demricknieladaza <demricknieladaza@gmail.com>
2018-08-23 00:34:40 -0700

    commit 08/22/2018
    wpa nahuman users and membership page..

--- resources/views/membership.blade.php
+++ resources/views/membership.blade.php
@@ -4,48 +4,101 @@
 
 	<div class="container-fluid below-header">
 	</div>
-	<div class="container-fluid lounge-wrapper">
-		<div class="row">
-			@include('sidemenu')
-			<div class="col-sm-10 col-sm-offset-2 lounge-main">
-				<div class="row">
-					<div class="col-sm-12 lounge-header">
-						<span>Membership</span>
-					</div>
+<div class="container-fluid lounge-wrapper">
+	<div class="row">
+		@include('sidemenu')
+		<div class="col-sm-10 col-sm-offset-2 lounge-main">
+			<div class="row">
+				<div class="col-sm-12 lounge-header">
+					<span>Membership</span>
 				</div>
-				<div class="row">
-					<div class="col-sm-12">
-						<div class="container" style="width: 100%;">
-							<div class="row">
-								<div class="col-sm-12">
-									<div class="tender-container tendnew">
-										<ul class="nav navs bid-form-nav">
-											<li class="active"><a class="abut" data-toggle="tab" href="#section1">My  Subscription </a></li>
-											<li><a class="abut" data-toggle="tab" href="#section2">My  Payment  Details </a></li>
-										</ul><br>
-									</div>
-								</div>
-								<div id="section1" class="tab-pane fade tender-container" style="padding-top:0;margin-left:auto;margin-right:auto;width:1040px;">
-									<div class="row">
-										<div class="container">
-											<h1 class="project-title bid-page-title">Design Responsibility Matrix</h1>
-										</div>
-									</div>
-								</div>
-								<div id="section2" class="tab-pane fade tender-container" style="padding-top:0;margin-left:auto;margin-right:auto;width:1040px;">
-									<div class="row">
-										<div class="container">
-											<h1 class="project-title bid-page-title">Design Responsibility Matrix</h1>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 project-anchor">
+					<a>My Subscription</a>
+				</div>
+			</div>
+			<div class="tender-container">
+				<div id="section1" class="tab-pane fade in active tender-container" style="border:none;">
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-sm-12 project-anchor">
+					<a>My Payment Details</a>
+				</div>
+			</div>
+			<div class="tender-container">
+				<div id="section1" class="tab-pane fade in active tender-container" style="border:none;">
+          <div class="container">
+              <div class="row">
+                  <div class="col-xs-12 col-md-4 col-md-offset-4">
+                      <div class="panel panel-default" style="border-color: transparent; ">
+                          <div class="panel-heading" style="background: transparent;color: transparent;">
+                              <div class="row">
+                                  <h3 class="text-center">Payment Details</h3>
+                                  <img class="img-responsive cc-img" src="http://www.prepbootstrap.com/Content/images/shared/misc/creditcardicons.png">
+                              </div>
+                          </div>
+                          <div class="panel-body">
+                              <form role="form">
+                                  <div class="row">
+                                      <div class="col-xs-12">
+                                          <div class="form-group">
+                                              <label>CARD NUMBER</label>
+                                              <div class="input-group">
+                                                  <input type="tel" class="form-control" placeholder="Valid Card Number" />
+                                                  <span class="input-group-addon"><span class="fa fa-credit-card"></span></span>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div class="row">
+                                      <div class="col-xs-7 col-md-7">
+                                          <div class="form-group">
+                                              <label><span class="hidden-xs">EXPIRATION</span><span class="visible-xs-inline">EXP</span> DATE</label>
+                                              <input type="tel" class="form-control" placeholder="MM / YY" />
+                                          </div>
+                                      </div>
+                                      <div class="col-xs-5 col-md-5 pull-right">
+                                          <div class="form-group">
+                                              <label>CV CODE</label>
+                                              <input type="tel" class="form-control" placeholder="CVC" />
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div class="row">
+                                      <div class="col-xs-12">
+                                          <div class="form-group">
+                                              <label>CARD OWNER</label>
+                                              <input type="text" class="form-control" placeholder="Card Owner Names" />
+                                          </div>
+                                      </div>
+                                  </div>
+                              </form>
+                          </div>
+                          <div class="panel-footer" style="background-color: transparent;border-top: none;">
+                              <div class="row">
+                                  <div class="col-xs-12">
+                                      <button class="btn btn-warning btn-lg btn-block">Process payment</button>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <style>
+              .cc-img {
+                  margin: 0 auto;
+              }
+          </style>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
 	
 	
 
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,32 +1,17 @@
 @extends('layouts.master')
 
 @section('content')
-	<style>
-		ul.user {
-		  list-style-type: none;
-		  padding: 0;
-		  margin: 0;
-		}
+<style>
+	.dropdown-submenu {
+	  position: relative;
+	}
 
-		ul.user li {
-		  border: 1px solid #ddd;
-		  margin-top: -1px; /* Prevent double borders */
-		  background-color: white;
-		  padding: 12px;
-		  text-decoration: none;
-		  width: 200px;
-		  font-size: 18px;
-		  color: black;
-		  display: block;
-		  position: relative;
-		}
-
-		ul.user li:hover, ul.user li:active, ul.user li:focus {
-		  background-color: #fe7235;
-		  color: white;
-		  font-size:bold;
-		}
-	</style>
+	.dropdown-submenu .dropdown-menu {
+    margin-left: 100%;
+    top:0;
+    width: 100%;
+	}
+</style>
 	<div class="container-fluid below-header">
 	</div>
 
@@ -40,19 +25,52 @@
 					</div>
 				</div>
 				<div class="row" style="background: white; margin:5px;padding-top: 15px;padding-bottom: 15px;box-shadow: 0 2px 4px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)!important">
-					<div class="col-sm-12">
+					<div class="col-sm-3">
 						<ul class="user">
-						  <li>Europe</li>
-						  	<ul class="dropdown-menu">
-				  	      <li><a href="#">HTML</a></li>
-				  	      <li><a href="#">CSS</a></li>
-				  	      <li><a href="#">JavaScript</a></li>
-				  	    </ul>
-						  <li>Americas</li>
-						  <li>Asia</li>
-						  <li>Africa</li>
-						  <li>Austria</li>
+					  	<li class="dropdown-submenu">
+					  		<a class="test" href="#">Europe</a>
+				        <ul class="dropdown-menu">
+				          <li><a tabindex="-1" href="#">Germany</a></li>
+				          <li class="dropdown-submenu">
+				            <a class="test" href="#">United Kingdom</a>
+				            <ul class="dropdown-menu">
+				              <li><a href="#">London - Aldgate</a></li>
+				              <li><a href="#">London - Wimbleton</a></li>
+				              <li class="dropdown-submenu">
+				                <a class="test" href="#">Manchester</a>
+				                <ul class="dropdown-menu">
+				                  <li><a href="#">Architecture</a></li>
+				                  <li><a href="#">Structural Engineering</a></li>
+				                </ul>
+				              </li>
+				            </ul>
+				          </li>
+				          <li><a tabindex="-1" href="#">France</a></li>
+				        </ul>
+				      </li>
+				      <li class="dropdown-submenu">
+					  		<a class="test" href="#">Americas</a>
+				        <ul class="dropdown-menu">
+				        </ul>
+				      </li>
+				      <li class="dropdown-submenu">
+					  		<a class="test" href="#">Asia</a>
+				        <ul class="dropdown-menu">
+				        </ul>
+				      </li>
+				      <li class="dropdown-submenu">
+					  		<a class="test" href="#">Africa</a>
+				        <ul class="dropdown-menu">
+				        </ul>
+				      </li>
 						</ul>
+						<div class="addblock1" style="display: none;">
+							<input type="text" id="inputAddtoList">
+							<button id="btnAddtoList">
+							Add to list
+							</button>
+						</div>
+						<a id="showadd" style="color:#fe7235">+ Add</a>
 					</div>
 				</div>
 			</div>
@@ -60,7 +78,32 @@
 	</div>
 
 	
-	
+	<script>
+	$(document).ready(function(){
+	  $('.test').on("click", function(e){
+	    $(this).next('ul').toggle();
+	    e.stopPropagation();
+	    e.preventDefault();
+	  });
+	});
+	</script>
+	<script>
+		$(function() {
+		$('#showadd').click(function(){
+		  $('.addblock1').css("display", "block");
+		  $(this).css("display", "none");
+		});
+		$('#btnAddtoList').click(function(){
+		  var text = $('#inputAddtoList').val();
+			var newDiv = $('<li class="dropdown-submenu"><a class="test" href="#">'+text+'</a><ul class="dropdown-menu"></ul></li>');
+		  //newDiv.style.background = "#000";
+		  $('.user').append(newDiv);
+		  $('#inputAddtoList').val('');
+		  $('.addblock1').css("display", "none");
+		  $('#showadd').css("display", "block");
+		});
+		});
+	</script>
 	<!-- end -->
 
 	
