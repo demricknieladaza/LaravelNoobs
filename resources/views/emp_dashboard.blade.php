86fe4cacd737948e8beb44e823fbc31024db1c0c
demricknieladaza <demricknieladaza@gmail.com>
2018-08-27 20:19:31 -0700

    commit
    jerald dako tiyan

--- resources/views/emp_dashboard.blade.php
+++ resources/views/emp_dashboard.blade.php
@@ -65,182 +65,195 @@
 						</div>	
 					</div>
 					<div class="col-sm-6 lounge-action">
-						<div class="shadow-wrapper">
-							<div class="panel-group pip" id="accordi">
-								<div class="panel">
-									<div id="collapse6" class="panel-collapse collapse in">
-										<div class="panel-body " style="height: 265px;">
-										<div class="col-sm-9">
-											<div class="angkol">
-												<a href=""><h2>Increase<br>  Discount</h2></a>
-											</div>
-											<div class="ante">
-												<a href=""><h2>Go<br>PRO+</h2></a>
-											</div>
-											    <div class="ate">
-												<a href=""><h2>Top<br>Up</h2></a>
-											</div>
-											 <div class="">
-												<input class="kuya" type="text" name="project" placeholder="Project Title">
-											</div>
-											 <div class="">
-												<button class="button nanay">Publish New Project</button>
-											</div>
-										</div>
-										<div class="col-sm-3">
-											 <div class="">
-											 	<label>You have</label>
-												<button class="button5">129</button>
-												<label class="manghud">Tender Credits</label>
-											</div>
-										</div>
+						<div class="shadow-wrapper col-cont">
+							<div class="col-sm-8">
+								<div class="row">
+									<div class="col-sm-4">
+										<div class="box">
+											<p>Increase Discount</p>
 										</div>
 									</div>
+									<div class="col-sm-4">
+										<div class="box">
+											<p>Go PRO+</p>
+										</div>
+									</div>
+									<div class="col-sm-4">
+										<div class="box">
+											<p>Top Up</p>
+										</div>
+									</div>
+								</div>
+								<div class="row" style="margin-top: 25px; padding-right: 10px; padding-left: 10px;">
+									<div class="form-group">
+										<input type="" name="" class="form-control" placeholder="Project Title" style="height: 40px; border: 3px solid black;">
+									</div>
+								</div>
+								<div class="row" style="margin-top: 25px; padding-right: 10px; padding-left: 10px;">
+									<div class="form-group">
+										<button class="btn col-sm-12 pnp" style="height: 50px;"><strong>Publish New Project</strong></button>
 									</div>
 								</div>
 							</div>
-						</div>	
+							<div class="col-sm-4">
+								<div class="titlehead">
+									<h3>You have</h3>
+								</div>
+								<div class="titlebody">
+									<div class="circ">
+										<h3 style="padding: 40% 0;">129</h3>
+									</div>
+								</div>
+								<div class="titlefooter">
+									<h3>
+										Tender Credits
+									</h3>
+								</div>
+							</div>
+						</div>
+					</div>	
+				</div>
+					<div class="row" style="margin-top: 25px; ">
+						<div class="col-sm-12">
+							<div class="shadow-wrapper lounge-demo">
+								<div class="form-group">
+									<a href="#collapse1">Bid New Project <span class="btn btn-warning remaining-tenders">Remaining Tender Credits 2</span></a>
+								</div>
+								<div class="form-group">
+									<h4>Active Projects  <span class="badge">2</span></h4>
+									<div class="row">
+										<div class="hover-container-holder">
+											<div class="tender-tile-single">
+												<div class="tender-tiles">
+													<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>		
+												</div>
+											</div>
+											<div class="tender-tile-single">
+												<div class="tender-tiles">
+													<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>
+												</div>
+											</div>
+											<div class="tender-tile-single">
+												<div class="tender-tiles">
+													<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>
+												</div>
+											</div>
+											<div class="row table-tender-details">
+												<div class="col-sm-6 table-left">
+													<table class="table table-hover table-striped">
+														<thead>
+															<tr>
+																<th>Active Projects</th>
+															</tr>
+														</thead>
+														<tbody>
+															<tr>
+																<td>Fire Engineer</td>
+															</tr>
+															<tr>
+																<td>Structural Engineer</td>
+															</tr>
+															<tr>
+																<td>Service Engineer</td>
+															</tr>
+															<tr>
+																<td>Architect</td>
+															</tr>
+															<tr>
+																<td>Facede Engineer</td>
+															</tr>
+														</tbody>
+													</table>
+												</div>
+												<div class="col-sm-6 table-right">
+													<table class="table table-hover table-striped">
+														<thead>
+															<tr>
+																<th>Drafted Projects</th>
+															</tr>
+														</thead>
+														<tbody>
+															<tr>
+																<td>Acoustic Engineer</td>
+															</tr>			
+														</tbody>
+													</table>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="form-group">
+									<h4>Closed Projects  <span class="badge">2</span></h4>
+									<div class="row">
+										<div class="hover-container-holder">
+											<div class="tender-tile-single">
+												<div class="tender-tiles">
+													<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>		
+												</div>
+											</div>
+											<div class="tender-tile-single">
+												<div class="tender-tiles">
+													<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>
+												</div>
+											</div>
+											<div class="tender-tile-single">
+												<div class="tender-tiles">
+													<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>
+												</div>
+											</div>
+											<div class="row table-tender-details">
+												<div class="col-sm-6 table-left">
+													<table class="table table-hover table-striped">
+														<thead>
+															<tr>
+																<th>Active Projects</th>
+															</tr>
+														</thead>
+														<tbody>
+															<tr>
+																<td>Fire Engineer</td>
+															</tr>
+															<tr>
+																<td>Structural Engineer</td>
+															</tr>
+															<tr>
+																<td>Service Engineer</td>
+															</tr>
+															<tr>
+																<td>Architect</td>
+															</tr>
+															<tr>
+																<td>Facede Engineer</td>
+															</tr>
+														</tbody>
+													</table>
+												</div>
+												<div class="col-sm-6 table-right">
+													<table class="table table-hover table-striped">
+														<thead>
+															<tr>
+																<th>Drafted Projects</th>
+															</tr>
+														</thead>
+														<tbody>
+															<tr>
+																<td>Acoustic Engineer</td>
+															</tr>			
+														</tbody>
+													</table>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>	
+						</div>
 					</div>
 				</div>			
 			</div>
 			<div class="col-sm-10 col-sm-offset-2 lounge-main">
-				<div class="row">
-					<div class="col-sm-12">
-						<div class="shadow-wrapper lounge-demo">
-							<div class="form-group">
-								<a href="#collapse1">Bid New Project <span class="btn btn-warning remaining-tenders">Remaining Tender Credits 2</span></a>
-							</div>
-							<div class="form-group">
-								<h4>Active Projects  <span class="badge">2</span></h4>
-								<div class="row">
-									<div class="hover-container-holder">
-										<div class="tender-tile-single">
-											<div class="tender-tiles">
-												<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>		
-											</div>
-										</div>
-										<div class="tender-tile-single">
-											<div class="tender-tiles">
-												<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>
-											</div>
-										</div>
-										<div class="tender-tile-single">
-											<div class="tender-tiles">
-												<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>
-											</div>
-										</div>
-										<div class="row table-tender-details">
-											<div class="col-sm-6 table-left">
-												<table class="table table-hover table-striped">
-													<thead>
-														<tr>
-															<th>Active Projects</th>
-														</tr>
-													</thead>
-													<tbody>
-														<tr>
-															<td>Fire Engineer</td>
-														</tr>
-														<tr>
-															<td>Structural Engineer</td>
-														</tr>
-														<tr>
-															<td>Service Engineer</td>
-														</tr>
-														<tr>
-															<td>Architect</td>
-														</tr>
-														<tr>
-															<td>Facede Engineer</td>
-														</tr>
-													</tbody>
-												</table>
-											</div>
-											<div class="col-sm-6 table-right">
-												<table class="table table-hover table-striped">
-													<thead>
-														<tr>
-															<th>Drafted Projects</th>
-														</tr>
-													</thead>
-													<tbody>
-														<tr>
-															<td>Acoustic Engineer</td>
-														</tr>			
-													</tbody>
-												</table>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="form-group">
-								<h4>Closed Projects  <span class="badge">2</span></h4>
-								<div class="row">
-									<div class="hover-container-holder">
-										<div class="tender-tile-single">
-											<div class="tender-tiles">
-												<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>		
-											</div>
-										</div>
-										<div class="tender-tile-single">
-											<div class="tender-tiles">
-												<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>
-											</div>
-										</div>
-										<div class="tender-tile-single">
-											<div class="tender-tiles">
-												<h2><a href="{{ url('/publish/project_dashboard') }}">Canada Water Masterplan</a></h2>
-											</div>
-										</div>
-										<div class="row table-tender-details">
-											<div class="col-sm-6 table-left">
-												<table class="table table-hover table-striped">
-													<thead>
-														<tr>
-															<th>Active Projects</th>
-														</tr>
-													</thead>
-													<tbody>
-														<tr>
-															<td>Fire Engineer</td>
-														</tr>
-														<tr>
-															<td>Structural Engineer</td>
-														</tr>
-														<tr>
-															<td>Service Engineer</td>
-														</tr>
-														<tr>
-															<td>Architect</td>
-														</tr>
-														<tr>
-															<td>Facede Engineer</td>
-														</tr>
-													</tbody>
-												</table>
-											</div>
-											<div class="col-sm-6 table-right">
-												<table class="table table-hover table-striped">
-													<thead>
-														<tr>
-															<th>Drafted Projects</th>
-														</tr>
-													</thead>
-													<tbody>
-														<tr>
-															<td>Acoustic Engineer</td>
-														</tr>			
-													</tbody>
-												</table>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>	
-					</div>
-				</div>
+				
 			</div>
 		</div>
 	</div>
